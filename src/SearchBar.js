11338e6c0bf7b57154c75039393a8dc01dc4148c
Sara Cemal <saracemal@gmail.com>
2021-04-30 23:02:12 -0400

    beginning search bar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function SearchBar() {
+const [searchedMovies, setsearchedMovies] = useState("")
     return (
         <div>
             <h1>Search Bar</h1>
